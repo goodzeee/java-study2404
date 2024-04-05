b196449d80dd6888dd70109fbd9c6db089e8945f
jihye <qkdtmdqo14@naver.com>
2024-04-05 12:27:03 +0900

    # java 변수 & 데이터 타입 학습

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
+import java.sql.SQLOutput;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+
+        System.out.println(10 + 5);
+        System.out.println("안녕 ?");
+
+        int month = 4;
+        int day = 10;
+        String vote = "국회의원 선거날";
+
+        System.out.println(month + "월 " + day + "일은 " + vote + "입니다."); //ln - line
+        System.out.printf("%d월 %d일은 %s입니다.\n", month, day, vote); //f - \n 따로 줄바꿈
     }
 }
