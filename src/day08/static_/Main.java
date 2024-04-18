360cbf2b849b286f0e694aff424acc702e50617c
jihye <qkdtmdqo14@naver.com>
2024-04-18 12:02:42 +0900

    # java 객체지향 - 사용 제한자 static, 싱글톤 패턴

--- src/day08/static_/Main.java
+++ src/day08/static_/Main.java
@@ -1,11 +1,15 @@
 package day08.static_;
 
+import util.SimpleInput;
+import static day08.static_.Count.*;
+import static util.SimpleInput.input;
+
 public class Main {
     public static void main(String[] args) {
 
 
         // static 데이터는 객체생성 없이 접근 가능 !
-        Count.x = 20; // 정적 필드
+        x = 20; // 정적 필드
         Count.m1();  // 정적 메서드  --> 객체생성 없으니 정적은 this xx
 
         Count c1 = new Count();
@@ -18,13 +22,32 @@ public class Main {
 
 //        c2.x = 99; // static int x; -> 값을 여러 개 받지 않고 하나로 통일된 정적 !!
 //        c1.x = 50;
-        Count.x = 99; // 어차피 값 1개이니 클래스명으로 가져오기
-        Count.x = 50;
+        x = 99; // 어차피 값 1개이니 클래스명으로 가져오기
+        x = 50;
 
         System.out.println("c1.y = " + c1.y);
         System.out.println("c2.y = " + c2.y);
 
-        System.out.println("c1.x = " + Count.x);
-        System.out.println("c2.x = " + Count.x);
+        System.out.println("c1.x = " + x);
+        System.out.println("c2.x = " + x);
+        System.out.println("==============================================");
+
+        Calculator redCal = new Calculator();
+        Calculator blueCal = new Calculator();
+
+        Calculator.calcArea(5); // 원주율 구하는 공식 메서드 static => 클래스명으로 하나 공유
+        redCal.paint("red");
+        blueCal.paint("blue");
+
+        //String color = input("색상 : ");
+
+        System.out.println("===============================================");
+
+        System.out.println(Person.nation);
+
+        Person kang = new Person("강지돌", 20);
+        System.out.println(kang.name);
+        System.out.println(kang.age);
+        System.out.println(kang.nation);
     }
 }
